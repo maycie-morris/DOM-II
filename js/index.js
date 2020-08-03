d2de1d81f7dc91a154e48dbd8232857af0de50f4
Maycie Morris <maycie@MacBook-Pro.local>
2020-08-02 20:21:35 -0700

    Created 6 events

--- js/index.js
+++ js/index.js
@@ -12,5 +12,51 @@ logo.addEventListener("mouseout", event => logo.style.fontSize = "4rem")
 
 const nav = document.querySelector("nav");
 
-nav.addEventListener("mouseenter", event => nav.style.borderBottom = "2px dotted blue");
-nav.addEventListener("mouseout", event => nav.style.borderBottom = "none");
+nav.addEventListener("keydownr", event => nav.style.borderBottom = "2px dotted blue");
+nav.addEventListener("keyupt", event => nav.style.borderBottom = "none");
+
+
+//3
+
+const headerImg = document.querySelector(".intro img");
+
+headerImg.addEventListener("click", event => headerImg.style.border = "10px dotted yellow");
+headerImg.addEventListener("dblclick", event => headerImg.style.border = "none");
+
+
+//4
+
+window.addEventListener("load", event => {
+    alert("You loaded the page!")}
+    );
+
+
+//5
+
+window.addEventListener("scroll", event => {
+    // alert("You're scrolling the page!");
+});
+
+
+//6
+
+const buttonBot = document.getElementsByClassName("div .btn")
+
+buttonBot.addEventListener("select", event => {
+    console.log("You've clicked a button!");
+});
+
+
+
+
+
+
+
+
+
+
+$('.nav').click(function (event){
+    event.preventDefault();
+    $('body,html').animate({
+      scrollTop: $($(this).attr('href')).offset().top - 100},2000);
+  });
