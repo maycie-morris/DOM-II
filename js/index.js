6bac1359b2acc432fac3cbeb7f09292d711aecbf
Maycie Morris <maycie@MacBook-Pro.local>
2020-08-02 19:41:25 -0700

    Completed 2 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-// Your code goes here
+//1
+
+const logo = document.querySelector("h1")
+
+logo.addEventListener("mouseenter", event => logo.style.color = "pink");
+logo.addEventListener("mouseenter", event => logo.style.fontSize = "6rem");
+logo.addEventListener("mouseout", event => logo.style.color = "black");
+logo.addEventListener("mouseout", event => logo.style.fontSize = "4rem")
+
+
+// 2
+
+const nav = document.querySelector("nav");
+
+nav.addEventListener("mouseenter", event => nav.style.borderBottom = "2px dotted blue");
+nav.addEventListener("mouseout", event => nav.style.borderBottom = "none");
