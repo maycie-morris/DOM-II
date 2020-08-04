e3680b7d5287e295fca1a724cff2b2858ac5d2ef
Maycie Morris <maycie@MacBook-Pro.local>
2020-08-04 13:15:23 -0700

    Added event listeners

--- js/index.js
+++ js/index.js
@@ -4,19 +4,20 @@ const logo = document.querySelector("h1")
 
 logo.addEventListener("mouseenter", event => logo.style.color = "pink");
 logo.addEventListener("mouseenter", event => logo.style.fontSize = "6rem");
+
+// 2 
 logo.addEventListener("mouseout", event => logo.style.color = "black");
 logo.addEventListener("mouseout", event => logo.style.fontSize = "4rem")
 
 
-// 2
+// 3
 
 const nav = document.querySelector("nav");
 
 nav.addEventListener("click", event => nav.style.borderBottom = "2px dotted blue");
-// nav.addEventListener("keyup", event => nav.style.borderBottom = "none");
 
 
-// 3
+// 4
 
 const headerImg = document.querySelector(".intro img");
 
@@ -24,25 +25,58 @@ headerImg.addEventListener("click", event => headerImg.style.border = "10px dott
 headerImg.addEventListener("dblclick", event => headerImg.style.border = "none");
 
 
-// 4
+// 5
 
 window.addEventListener("load", event => {
     alert("You loaded the page!")}
     );
 
 
-// 5
-
-window.addEventListener("scroll", event => {
-    // alert("You're scrolling the page!");
-});
-
-
 // 6
 
-const funBus = h1.addEventListener('mousedown', e => {
-    h1.textContent = 'What a fun bus!'
+let text = document.querySelector('.footer p');
+window.addEventListener("scroll", e => {
+  text.style.backgroundColor = "green";
 });
+
+
+// 7
+
+let funBus = document.querySelector('h1')
+
+funBus.addEventListener('mousedown', e => {
+    funBus.textContent = 'What a fun bus!'
+});
+
+
+// 8
+
+window.addEventListener("resize", e => {
+    alert("Is it getting smaller in here?");
+
+  });
+
+// 9
+
+let allImg = document.querySelector("img")
+
+allImg.addEventListener("drag", e => {
+    allImg.src = "https://images.unsplash.com/photo-1577459640575-219cbf231b8b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1254&q=80"
+})
+
+
+  // 10
+
+  let h2 = document.querySelector('h2')
+
+  h2.addEventListener('mousemove', e => {
+    h2.style.textShadow = '2px 2px 8px #000000'
+    
+})
+
+
+  
+
 
 
 
